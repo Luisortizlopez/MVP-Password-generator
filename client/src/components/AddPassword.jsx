d5b5e8806c33052d02aacc116b72ff66ea892a00
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-02-14 18:29:05 -0600

    working on server connection

--- client/src/components/AddPassword.jsx
+++ client/src/components/AddPassword.jsx
@@ -1,1 +1,7 @@
 //This file will be used to capture password generated from (GeneratePassword)
+render(){
+<div>
+RandomPassword: <button onClick={}>GeneratePassword</button>
+</div>
+export default AddPassword;
+}
---
+++ client/src/index.jsx
@@ -0,0 +1,2 @@
+import React from 'react'
+import AddPassword from './AddPassword.jsx'
